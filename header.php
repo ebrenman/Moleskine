53311e96d19c6751e726103e9c91cb5c839d9fbf
Esteban Brenman <ebrenman@gmail.com>
2021-05-01 08:39:37 -0300

    2021-05-01 Embellecimiento de codigo

---
+++ header.php
@@ -0,0 +1,25 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="description" content="ebrenman - Mi Moleskine" />
+    <meta name="keywords" content="book, flip, pages, moleskine, drawings,"/>
+    <meta property="og:image" content="images/share.png" />
+	<link rel="shortcut icon" href="../favicon.ico" type="image/x-icon"/>
+
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.4.min.js"></script>
+	<script src="booklet/jquery.easing.1.3.js" type="text/javascript"></script>
+	<script src="booklet/jquery.booklet.1.1.0.min.js" type="text/javascript"></script>
+
+	<link href="booklet/jquery.booklet.1.1.0.css" type="text/css" rel="stylesheet" media="screen" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen"/>
+
+	<script src="cufon/cufon-yui.js" type="text/javascript"></script>
+	<script src="cufon/ChunkFive_400.font.js" type="text/javascript"></script>
+	<script src="cufon/Note_this_400.font.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		Cufon.replace('h1,p,.b-counter');
+		Cufon.replace('.book_wrapper a', {hover:true});
+		Cufon.replace('.title', {textShadow: '1px 1px #C59471', fontFamily:'ChunkFive'});
+		Cufon.replace('.reference a', {textShadow: '1px 1px #C59471', fontFamily:'ChunkFive'});
+		Cufon.replace('.loading', {textShadow: '1px 1px #000', fontFamily:'ChunkFive'});
+	</script>
+</head>
